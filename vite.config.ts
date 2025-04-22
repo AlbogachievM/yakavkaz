420c969878110573c26d44612b1c23e54d6617c3
albogachiev <albogachiev-2014@mail.ru>
2025-04-22 14:16:09 +0300

    fix config vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(),  tsconfigPaths()],
-  base:'https://AlbogachievM.github.io/yakavkaz/',
+  base:'/yakavkaz/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
