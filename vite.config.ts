7f2d781c32a4cd469d5c51af6bd57d4fa9d00ccd
albogachiev <albogachiev-2014@mail.ru>
2025-04-22 14:54:22 +0300

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(),  tsconfigPaths()],
-  base:'/yakavkaz/',
+  base:'/yakavkaz',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
